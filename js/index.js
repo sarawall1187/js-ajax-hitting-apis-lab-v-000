3b927ff2f4006dd9a7fa5315ee0508869d1dd4c0
Sara Walker <sarawall1187@gmail.com>
2019-06-14 00:49:28 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // your code here
 function displayRepositories() {
   var repos = JSON.parse(this.responseText);
-  console.log(repos);
+  // console.log(repos);
   const repoList = `<ul>${repos
     .map(r => '<li>' + r.clone_url + '</li>')
     .join('')}</ul>`;
@@ -19,23 +19,45 @@ function getRepositories() {
 
 function displayCommits() {
   const commits = JSON.parse(this.responseText);
-  const commitsList = `<ul>${commits
-    .map(
-      commit =>
-        '<li><strong>' +
-        commit.author.login +
-        '</strong> - ' +
-        commit.commit.message +
-        '</li>'
-    )
-    .join('')}</ul>`;
-  document.getElementById('commits').innerHTML = commitsList;
+    const commitsList = `<ul>${commits
+      .map(
+        commit =>
+          '<li>' +
+          commit.commit.author.name + ' - ' + commit.commit.message + ' ' + commit.committer.login +
+          '</li>'
+      )
+      .join('')}</ul>`;
+    document.getElementById('details').innerHTML = commitsList;
 }
 
 function getCommits(el) {
-  const name = el.dataset.repo;
+    const name = el.dataset.repository;
+
+    const req = new XMLHttpRequest();
+    req.addEventListener('load', displayCommits);
+    req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/commits');
+    req.send();
+}
+
+function displayBranches(){
+  const branches = JSON.parse(this.responseText);
+  console.log(branches)
+  const branchList = `<ul>${branches.map(branch => '<li>' + branch.commit.url + '</li>').join('')}</ul>`
+  // const commitsList = `<ul>${commits
+  //   .map(
+  //     commit =>
+  //       '<li>' +
+  //       commit.commit.author.name + ' - ' + commit.commit.message + ' ' + commit.committer.login +
+  //       '</li>'
+  //   )
+  //   .join('')}</ul>`;
+  document.getElementById('details').innerHTML = branchList;
+}
+
+function getBranches(){
   const req = new XMLHttpRequest();
-  req.addEventListener('load', displayCommits);
-  req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/commits');
+  req.addEventListener('load', displayBranches);
+  req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/branches');
   req.send();
+
 }
